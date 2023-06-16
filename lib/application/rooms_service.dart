434ba119a028f1e3f42a3572a54903861a286abe
henriqwuchryn <henriq.gwm@gmail.com>
2023-06-16 14:43:46 -0300

    commit2

--- lib/application/rooms_service.dart
+++ lib/application/rooms_service.dart
@@ -2,8 +2,11 @@ import '../mock/mock_rooms.dart';
 import '../model/room.dart';
 
 class RoomsService {
-  List<Room> getRooms(){
+  List<Room> getRooms() {
     return mockRooms;
   }
-}
 
+  void addRoom(Room room) {
+    mockRooms.add(room);
+  }
+}
